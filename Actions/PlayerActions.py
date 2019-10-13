b9726d1bbe0bc428a4d4f736b934cd93c354b8c3
EsmarlynDiaz <Esmarlyndiaz23@gmail.com>
2019-10-13 17:53:15 -0400

    Changed playerActions

--- Actions/PlayerActions.py
+++ Actions/PlayerActions.py
@@ -1,5 +1,6 @@
 #
 
+
 def playPerson(player, table, passNum):
     print('is the turn of: ', player.name)
     input('press enter to continue')
@@ -11,13 +12,17 @@ def playPerson(player, table, passNum):
         passNum[0] = 0
         print('you can play a token')
         print('enter the number: ', end='')
-        while not player.addTokenToTable(input(), table):
+        token = input()
+        while not player.addTokenToTable(token, table):
             print('enter a valid token: ', end='')
+            token = input()
+        print(player.name + ' has played: ' + token)
     else:
         print('you can not play a token')
         input('press enter to pass')
         player.passTurn()
         passNum[0] += 1
+        print(player.name + ' has played: passed')
 
 
 def playBot(player, table, handPlay, passNum):
@@ -25,6 +30,8 @@ def playBot(player, table, handPlay, passNum):
     if player.checkPlay(table):
         player.autoPlay(table, handPlay)
         passNum[0] = 0
+        # print(player.name + ' has played: ')
     else:
         player.passTurn()
         passNum[0] += 1
+        print(player.name + ' has played: passed')
